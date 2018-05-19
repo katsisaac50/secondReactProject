a8f51106db5893f63ce16e09b71c986a32f68d5a
isaac <katsisaac50@gmail.com>
2018-05-19 18:25:18 +0200

    Added firebase module to fetch Json data

--- src/App.js
+++ src/App.js
@@ -8,6 +8,28 @@ import Portfolio from './Components/Portfolio';
 import Testimonials from './Components/Testimonials';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
+var uuid=require('uuid');
+var firebase=require('firebase');
+
+var config = {
+  apiKey: "AIzaSyBF23hOulrSNxTJD4a7SrQuKdeiLwhAfx0",
+  authDomain: "simplesurvey-371ce.firebaseapp.com",
+  databaseURL: "https://reactdata-5f8b5.firebaseio.com/",
+  projectId: "simplesurvey-371ce",
+  storageBucket: "simplesurvey-371ce.appspot.com",
+  messagingSenderId: "968873690005"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var ref = database.ref();
+ref.on("value", function(snapshot) {
+  let data=JSON.stringify(snapshot.val().sugar);
+   console.log(snapshot.val().sugar);
+}, function (error) {
+   console.log("Error: " + error.code);
+});
 
 class App extends Component {
   constructor(props){
