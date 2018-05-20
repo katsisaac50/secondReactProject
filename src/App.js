899c2c1ec6b2e929db4c262d979344aa0de34eac
Ahmad Magdy <magdy.a@hotmail.com>
2018-05-20 03:10:07 +0200

    Comment out the firebase code, uncomment the ajax code and change the url to absolute url (removing localhost:3000)

--- src/App.js
+++ src/App.js
@@ -9,28 +9,28 @@ import Testimonials from './Components/Testimonials';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
 var uuid=require('uuid');
-var firebase=require('firebase');
+// var firebase=require('firebase');
 
-var config = {
-  apiKey: "AIzaSyBF23hOulrSNxTJD4a7SrQuKdeiLwhAfx0",
-  authDomain: "simplesurvey-371ce.firebaseapp.com",
-  databaseURL: "https://reactdata-5f8b5.firebaseio.com/",
-  projectId: "simplesurvey-371ce",
-  storageBucket: "simplesurvey-371ce.appspot.com",
-  messagingSenderId: "968873690005"
-};
-firebase.initializeApp(config);
+// var config = {
+//   apiKey: "AIzaSyBF23hOulrSNxTJD4a7SrQuKdeiLwhAfx0",
+//   authDomain: "simplesurvey-371ce.firebaseapp.com",
+//   databaseURL: "https://reactdata-5f8b5.firebaseio.com/",
+//   projectId: "simplesurvey-371ce",
+//   storageBucket: "simplesurvey-371ce.appspot.com",
+//   messagingSenderId: "968873690005"
+// };
+// firebase.initializeApp(config);
 
-var database = firebase.database();
+// var database = firebase.database();
 
-var ref = database.ref();
-ref.on("value", function(snapshot) {
-  let data=JSON.stringify(snapshot.val().sugar);
-   console.log(snapshot.val().sugar);
-   this.setState({resumeData: data});
-}.bind(this), function (error) {
-   console.log("Error: " + error.code);
-});
+// var ref = database.ref();
+// ref.on("value", function(snapshot) {
+//   let data=JSON.stringify(snapshot.val().sugar);
+//    console.log(snapshot.val().sugar);
+//    this.setState({resumeData: data});
+// }.bind(this), function (error) {
+//    console.log("Error: " + error.code);
+// });
 class App extends Component {
   constructor(props){
     super(props);
@@ -41,26 +41,26 @@ class App extends Component {
   }
 
   getResumeData(){
-    // $.ajax({
-    //   url:'http://localhost:3000/resumeData.json',
-    //   dataType:'json',
-    //   cache: false,
-    //   success: function(data){
-    //     console.log(data);
-    //     this.setState({resumeData: data});
-    //   }.bind(this),
-    //   error: function(xhr, status, err){
-    //     console.log(err);
-    //     alert(err);
-    //   }
-    // });
-    ref.on("value", function(snapshot) {
-      let data=snapshot.val().sugar;
-       console.log(snapshot.val().sugar);
-       this.setState({resumeData: data});
-    }.bind(this), function (error) {
-       console.log("Error: " + error.code);
-    }); 
+    $.ajax({
+      url:'/resumeData.json',
+      dataType:'json',
+      cache: false,
+      success: function(data){
+        console.log(data);
+        this.setState({resumeData: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+        alert(err);
+      }
+    });
+    // ref.on("value", function(snapshot) {
+    //   let data=snapshot.val().sugar;
+    //    console.log(snapshot.val().sugar);
+    //    this.setState({resumeData: data});
+    // }.bind(this), function (error) {
+    //    console.log("Error: " + error.code);
+    // }); 
   }
 
   componentDidMount(){
